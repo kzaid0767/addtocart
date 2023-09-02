56a21950445f846b4c1e53c80b279ffc0917e5cb
Kassim Zaid <81573513+kzaid0767@users.noreply.github.com>
2023-09-02 13:43:27 -0400

    icons added with favicon io

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL : "https://playground-d1d7a-default-rtdb.firebaseio.com/"
@@ -8,17 +8,63 @@ const appSettings = {
 
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
-const itemsinCart = ref(database,"cartitems")
+const itemsInDB = ref(database,"cartitems")
 
 const inputEl = document.querySelector('#input-field')
 const btn = document.querySelector('#add-button')
-
 btn.addEventListener('click',handleClick)
+//ul holder for list items
+const shoppingList = document.querySelector('#shopping-list')
+
+//fetching from firebase
+onValue(itemsInDB, function(snapshot){
+    //making array from snapshot values
+    if(snapshot.exists()){
+        let itemsArray = Object.entries(snapshot.val())
+        // console.log(itemsArray)
+        //reset innerhtml
+        clearShoppingListEl()
+        let newInnerHTML = ``
+        for(let thing of itemsArray){
+            let currentItemID = thing[0]
+            let currentItemValue = thing[1]
+            addULHtml(thing)
+        }
+    } else shoppingList.innerHTML = `<li> cart is empty!</li>`
+})
 
 function handleClick(){
     let inputValue = inputEl.value
 
     //pushing to the database in firebase
-    push(itemsinCart,inputValue)
-    console.log(inputValue)
+    if(inputValue){
+        push(itemsInDB,inputValue)
+    }
+
+    resetInputEl()    
+    //addULHtml(inputValue)
+    
+}
+
+function resetInputEl(){
+    inputEl.value = ''
+}
+
+function clearShoppingListEl(){
+    shoppingList.innerHTML = ''
+}
+
+
+function addULHtml(value){
+    let [itemID,itemName] = value
+    let newEl = document.createElement('li')
+    newEl.textContent = itemName
+    newEl.addEventListener("click",()=>deleItem(itemID))
+    shoppingList.append(newEl)
+}
+
+//handle deleting from database
+function deleItem(itemID){
+    let exactLocationInFirebaseDB = ref(database,`cartitems/${itemID}`)
+    remove(exactLocationInFirebaseDB)
 }
